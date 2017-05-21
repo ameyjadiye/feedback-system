9a12b3a7ba90c9cd5e8b3455e09c8ea6dbbca847
Amey Jadiye <ameyjadiye@gmail.com>
2017-05-21 22:54:03 +0530

    added feedback and detail table to  feedback.sql

--- feedback.sql
+++ feedback.sql
@@ -16,6 +16,60 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `fb_feedback_102`
+--
+
+DROP TABLE IF EXISTS `fb_feedback_102`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fb_feedback_102` (
+  `id` bigint(12) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fb_feedback_102`
+--
+
+LOCK TABLES `fb_feedback_102` WRITE;
+/*!40000 ALTER TABLE `fb_feedback_102` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fb_feedback_102` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fb_feedback_details_103`
+--
+
+DROP TABLE IF EXISTS `fb_feedback_details_103`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fb_feedback_details_103` (
+  `id` bigint(12) NOT NULL AUTO_INCREMENT,
+  `feedback_id` bigint(12) NOT NULL,
+  `to_user_id` bigint(12) DEFAULT NULL,
+  `from_user_id` bigint(12) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_fb_feedback_details_103_2_idx` (`to_user_id`),
+  KEY `fk_fb_feedback_details_103_3_idx` (`from_user_id`),
+  CONSTRAINT `fk_fb_feedback_details_103_2` FOREIGN KEY (`to_user_id`) REFERENCES `fb_user_100` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fb_feedback_details_103_3` FOREIGN KEY (`from_user_id`) REFERENCES `fb_feedback_102` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fb_feedback_details_103`
+--
+
+LOCK TABLES `fb_feedback_details_103` WRITE;
+/*!40000 ALTER TABLE `fb_feedback_details_103` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fb_feedback_details_103` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `fb_group_101`
 --
 
@@ -76,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-20 23:38:32
+-- Dump completed on 2017-05-22 22:53:01
