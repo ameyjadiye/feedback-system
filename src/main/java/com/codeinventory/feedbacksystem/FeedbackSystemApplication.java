ad2a6b1694205da08c01b90c630a2aa5b8063614
Amey Jadiye <ameyjadiye@gmail.com>
2019-03-03 20:04:19 +0530

    adding spring boot base and maven wrapper

---
+++ src/main/java/com/codeinventory/feedbacksystem/FeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeinventory.feedbacksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeinventory/feedbacksystem/FeedbackSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeinventory.feedbacksystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeedbackSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
