31428e38ac5fd8ac23bc333a083a15e29e04925c
Amey Jadiye <ameyjadiye@gmail.com>
2019-03-05 22:36:56 +0530

    adding angular ui base

---
+++ feedback-service/src/main/java/com/codeinventory/feedbacksystem/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeinventory.feedbacksystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+       @GetMapping
+       public String home(Model model) {
+              return "forward:/index.html";
+       }
+}
